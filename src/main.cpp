7ca71bf58da49d2eb45704153923506445444a05
Elvandry Ghiffary <elvandry@warungpintar.co>
2020-07-01 21:29:40 +0700

    add lcd interface

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
  */
 #include <Arduino.h>
 #include "ultrasonic.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 // IN ultrasonic pins
 #define trigPinU1 2
@@ -20,6 +24,7 @@
 #define echoPinU2 5
 
 int person = 0;
+int lastPerson = 0;
 
 void setup()
 {
@@ -29,6 +34,13 @@ void setup()
 	// Ultrasonic setup pin
 	ultrasonic_setup_pin(trigPinU1, echoPinU1);
 	ultrasonic_setup_pin(trigPinU2, echoPinU2);
+
+	// LCD init
+	lcd.begin(16,2);
+	lcd.clear();
+	lcd.setCursor(0,0);
+  	lcd.print(" People Counter ");
+	delay(2000);
 }
 
 void loop()
@@ -46,4 +58,15 @@ void loop()
 		person--;
 		Serial.println(person);
 	}
+
+	// Print to LCD display if person changed
+	if (person != lastPerson)
+	{
+		lcd.clear();
+		lcd.setCursor(0,0);
+		lcd.print("Total person :");
+		lcd.setCursor(0,1);
+		lcd.print(person);
+	}
+	lastPerson = person;
 }
